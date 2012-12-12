463c8c5236a352a9fdb5948c6de2eba720b1b8af
Darius Bacon <darius@wry.me>
2012-12-12 16:41:19 -0500

    add module doc

--- pythological.py
+++ pythological.py
@@ -1,3 +1,9 @@
+"""
+A port of core Minikanren to Python from William Byrd's dissertation chapter 3.
+The goal part doesn't try to follow him in any detail, using Python generators
+instead.
+"""
+
 from itertools import count, islice
 
 # Top level
